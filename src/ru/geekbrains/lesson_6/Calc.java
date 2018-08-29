c34b34010013f86c14897a766def158c0de10d73
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-30 00:09:21 +0500

    finish basic form

--- src/ru/geekbrains/lesson_6/Calc.java
+++ src/ru/geekbrains/lesson_6/Calc.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.lesson_6;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Calc extends JFrame {
+
+    private double result;
 
     public Calc(){
 
@@ -11,13 +14,34 @@ public class Calc extends JFrame {
         setLocation(100,100);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        JPanel insert = new JPanel(new GridLayout(1, 2, 10, 0));
+        insert.add(new JTextField(15));
+        insert.add(new JTextField(15));
 
+        JPanel operations = new JPanel(new GridLayout(1, 5, 5, 0));
+        operations.add(new JButton("+"));
+        operations.add(new JButton("-"));
+        operations.add(new JButton("*"));
+        operations.add(new JButton("/"));
+        operations.add(new JButton("exponentiation"));
+
+        JPanel oper = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        oper.add(operations);
+
+        JPanel contents = new JPanel(new BorderLayout());
+        contents.add(insert, BorderLayout.NORTH);
+        contents.add(oper, BorderLayout.CENTER);
+        //contents.add(new JLabel("Result: " + result), BorderLayout.SOUTH);
+
+        Container container = getContentPane();
+        container.add(contents);
+
+        pack();
+        setVisible(true);
     }
 
     public static void main(String[] args) {
 	// write your code here
-        Calc app = new Calc();
-        app.setVisible(true);
-        app.pack();
+        new Calc();
     }
 }
